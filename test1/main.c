b5585f0cd3c11b5d398c1a0ddf66a39b1bd7bc80
govindarajulu <govindarajulu90@gmail.com>
2012-05-22 19:44:57 +0530

    / ‘test1/main.c’

--- test1/main.c
+++ test1/main.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <dlfcn.h>
 
-int spawn(const char* program,const char* arglist[]){
+int spawn(const char* program,char* arglist[]){
     int pid;
     pid=fork();
     if(pid==0){
@@ -19,7 +19,7 @@ int spawn(const char* program,const char* arglist[]){
 
 int main(int argc, char* argv[])
 {
-    const char* arglist[]={"ls","-l"};
+    char* arglist[]={"ls","-l"};
     char* program="ls";
     spawn(program,arglist);
     return 0;
