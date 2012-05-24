eb7c443fc6256f0fb3e21ec0604b8f2334087232
govindarajulu <govindarajulu90@gmail.com>
2012-05-24 22:07:52 +0530

    / ‘test1/main.c’ / ‘test1/main.o’ / ‘test1/test1’

--- test1/main.c
+++ test1/main.c
@@ -21,7 +21,6 @@ int main(int argc, char* argv[])
     memset(&sa,0,sizeof(sa));
     sa.sa_handler=sig_handler;
     sa.sa_flags=SA_RESTART;
-    sa.sa_restorer=
     sigemptyset(&sa.sa_mask);
     sigaction(SIGINT,&sa,NULL);
     printf("sleeping for 20 sec\n");
