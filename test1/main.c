db0ed5b3f6db333243ae5e78ff264e14ee9074b6
govindarajulu <govindarajulu90@gmail.com>
2012-05-24 21:53:39 +0530

    / ‘test1/main.c’ / ‘test1/main.o’ / ‘test1/test1’

--- test1/main.c
+++ test1/main.c
@@ -16,7 +16,7 @@ void sig_handler(int sig_num){
     sigaction(SIGINT,&sa,NULL);
     printf("ctrl+c pressed!!!\n");
     ++count;
-    signal(sig_num,sig_handler);
+
 }
 
 int main(int argc, char* argv[])
