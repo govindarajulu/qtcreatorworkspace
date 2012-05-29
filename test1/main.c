3fb794c031d82bcba67c715350dbc7c5b0f76c94
govindarajulu <govindarajulu90@gmail.com>
2012-05-29 20:27:50 +0530

    / ‘test1/main.c’

--- test1/main.c
+++ test1/main.c
@@ -3,15 +3,13 @@
 #include <string.h>
 char* thread(void* nouse){
     char a[10];
-    int b;
-    b=17;
     strcpy(a,"poison");
     printf("a=%s\n",a);
-    return &b;
+    return a;
 }
 
 main(){
-    int *a;
+    char *a;
     a=thread(NULL);
-    printf("%d\n",*a);
+    printf("==%s\n",*a);
 }
