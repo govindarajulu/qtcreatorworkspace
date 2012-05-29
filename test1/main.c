1e5d59a95db15dd6ca51426891c95b37e530b5bf
govindarajulu <govindarajulu90@gmail.com>
2012-05-29 20:23:52 +0530

    / ‘test1/main.c’ - ‘test1/main.c.autosave’

--- test1/main.c
+++ test1/main.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 #include <pthread.h>
-void* thread(void* nouse){
-    while(1){
-        int i;
-        i++;
-    }
-    pthread_exit(NULL);
+#include <string.h>
+char* thread(void* nouse){
+    char a[10];
+    strcpy(a,"poison");
+    return a;
 }
 
 main(){
-    pthread_t id;
-    pthread_create(&id,NULL,*thread,NULL);
-    pthread_t id2;
-    pthread_create(&id2,NULL,*thread,NULL);
-    pthread_t id3;
-    pthread_create(&id3,NULL,*thread,NULL);
-    while(1){
-        pthread_join(id,NULL);
-        int j;
-        j++;
-        //printf("y");
-    }
+    char *a;
+    a=thread(NULL);
+    printf("%s\n",a);
 }
