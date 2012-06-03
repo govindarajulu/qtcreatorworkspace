04a6610d0952b82ea676fc6b3ba133c46269b281
govindarajulu <govindarajulu90@gmail.com>
2012-06-03 19:14:58 +0530

    / ‘test1/main.c’

--- test1/main.c
+++ test1/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
+#include <error.h>
 void* thread(void* nouse){
     return NULL;
 }
@@ -8,7 +9,9 @@ void* thread(void* nouse){
 main(){
     pthread_t th,th1;
     pthread_create(&th,NULL,thread,NULL);
+    perror("pthread_create");
     pthread_create(&th1,NULL,thread,NULL);
+    perror("pthread_create");
     printf("th id in main()=%d",(int)th);
     printf("th1 id in main()=%d",(int)th1);
 }
